1ff8bcdc67d5067874ca28acbb1bd8f59c14caa4
Gerardo J. Rito <grrdrito@gmail.com>
2026-02-25 13:06:40 -0600

    chore: initial project setup with Vite and Tailwind

---
+++ Alineacion y balanceo Rito's/src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+    return (
+        <div className="min-h-screen flex items-center justify-center bg-black">
+            <h1 className="text-4xl font-bold text-cyan-400">
+                Tailwind v4 funcionando 🚀
+            </h1>
+        </div>
+    )
+}
