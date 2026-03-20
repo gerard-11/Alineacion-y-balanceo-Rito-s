caa1f1f90fda38cc2802d4926c2bc50b0f91f67d
Gerardo J. Rito <grrdrito@gmail.com>
2026-03-19 21:02:14 -0600

    chore: temporarily disable MSW initialization

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import './index.css'
+
+// TODO: MSW temporarily disabled
+// Uncomment when ready to use mock service worker
+/*
+if (import.meta.env.DEV && import.meta.env.VITE_LOCAL !== 'true') {
+  const { worker } = await import('./mocks/worker')
+  await worker.start({
+    onUnhandledRequest: 'bypass',
+  })
+}
+*/
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
